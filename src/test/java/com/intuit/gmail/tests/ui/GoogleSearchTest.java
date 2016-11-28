daf6466ebdb394a85a37b1ab7d6797ae58bff067
dlin1 <dennis_lin@intuit.com>
2016-11-28 13:47:36 -0800

    Adding Chrome driver.

--- src/test/java/com/intuit/gmail/tests/ui/GoogleSearchTest.java
+++ src/test/java/com/intuit/gmail/tests/ui/GoogleSearchTest.java
@@ -1,14 +1,16 @@
 package com.intuit.gmail.tests.ui;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class GoogleSearchTest {
 
 	@Test
 	public void test() {
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "chromedriver.mac");
+		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com/");
 	}
 }
